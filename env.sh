242b68b479a2f6afd3c379033041ff67d794eeb0
kuailedeluojie <812788375@qq.com>
2020-07-23 10:01:18 +0000

    更新netopeer2启动脚本

--- env.sh
+++ env.sh
@@ -4,3 +4,7 @@ cd /opt/all/sysrepo/bin
 export PATH=$PWD:$PATH
 cd /opt/all/libyang/bin
 export PATH=$PWD:$PATH
+export YANG_MODULE_DIR=/opt/all/netopeer2/share/yang/modules/netopeer2
+export MODULES_PERMS=660
+export NP2_MODULE_OWNER=root 
+export NP2_MODULE_GROUP=netconf
