9d96102355ad351e0dd6acb046a57433508d6c70
kuailedeluojie <812788375@qq.com>
2020-08-14 10:22:59 +0000

    交叉编译sysrepo的例程

---
+++ sysrepo/examples/build/CMakeFiles/application_changes_example.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mo/share/netconf2_new/sysrepo/examples/application_changes_example.c" "/home/mo/share/netconf2_new/sysrepo/examples/build/CMakeFiles/application_changes_example.dir/application_changes_example.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/sysrepo/include"
+  "/opt/libyang/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sysrepo/examples/build/CMakeFiles/oper_data_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oper_data_example.dir/oper_data_example.c.o"
+  "oper_data_example"
+  "oper_data_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/oper_data_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
